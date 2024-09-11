1350689ca43a905553f109b0516c78fda72d2ac4
anapaulacd <anapaulacoqueirodogado@gmail.com>
2024-09-11 12:19:13 -0300

    Adicionei comentarios.

--- controller/autocontroller.php
+++ controller/autocontroller.php
@@ -1,5 +1,5 @@
 <?php
-// requer 
+// requer arquivo 'user.php que contem o model user com as função para a maipulação de dados do usuarios
 require_once 'models/user.php';
 
 class AuthController
@@ -7,7 +7,7 @@ class AuthController
     // Criar função reponsavel pelo processo de login
     public function login()
     {
-        
+
     }
 }
 ?>
--- routes.php
+++ routes.php
@@ -2,16 +2,16 @@
 // include arquivos de controlador para lidar com diferentes ações 
 require 'controlles/AuthController.php'; // inclui o controlador de autenticação 
 require 'controlles/userController.php'; //inclui colaborador de usuario 
-require 'controlles/dashboardController.php'; //
-//
-$autocontroller = new AuthController(); //
+require 'controlles/dashboardController.php'; //inclui colaborador de dashboard.
+// Criar instancia dos controladores para utilizar seus métodos.
+$autocontroller = new AuthController(); // Instancia o controlador de autenticação= para poder usar 
 
-//
-$action = $_GET['action'] ?? 'ligin'; //
+// Coletar a ação url, se não ouver definida, usar 'login' por padrão 
+$action = $_GET['action'] ?? 'ligin'; // Usar operedor de colescencia nula (??) para definir 'login' se 'action' não estiver presente 
 
 switch($action){
     case 'iogin':
-        $autocontroller->login(); //
+        $autocontroller->login(); // Chama o método login do controlador de autenticação.
     }
 
 
