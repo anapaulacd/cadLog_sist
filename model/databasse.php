0258afa28a283fc7d6c61b656569f072af6d889a
anapaulacd <anapaulacoqueirodogado@gmail.com>
2024-09-18 10:06:21 -0300

    Método público  que retorna a conexão com Banco de dados.

---
+++ model/databasse.php
@@ -0,0 +1,19 @@
+<?php 
+class database{
+    //utiliza padrao singleton, cujo o objeto é 
+    private static $instance = null;
+    //Método público  que retorna a conexão com bd
+    public static function getConnection(){
+if(!self::$instance){
+    $host    = 'localhost';
+    $db     = 'sistema_usuarios';
+    $password  = '';
+    //A conexão usa o driver Mysql (mysql;)e as informaçãoes de host e bd
+    self::$instance = new pdo("msql:host=$host;dbname=$db", $user, $password);
+    // Define o modo de erro para e
+    self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+return self::$instance;
+    }
+}
+?>
