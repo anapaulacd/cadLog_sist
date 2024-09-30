362ff0a9b0574f1fb0b41fb8b7109bd341df3b62
anapaulacd <anapaulacoqueirodogado@gmail.com>
2024-09-30 08:20:07 -0300

    adicionei o readme e img.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # cadLog_sist
+
+## Descrição
+ 
+ Esse é um projeto que foi feito em sala, nas aulas de programação web no ano de 2024, com a orientação do professor Leonardo Rocha para aumentar o comhecimento e introduzir novos aprendizados. Esse é um projeto que permite com que o usuário realize  cadastro e login no sistema. Estamos aprendendo conseitos de php esse sistema é ligado com o banco de dados
+ 
+ 
+ 
+## Funcionalidades
+ * O usuário pode fazer cadastro
+ * O usuário pode faszer login
+ 
+ 
+## 💻 tecnologias utilizadas
+ 
+* VsCode
+ * HTML
+ * Github
+ * CSS
+* php
+ 
+## 📄 Aprendizados
+Nessa atividade usamos conceitos básicos da linguagem PHP para fazer o backend e ligar a aplicação com banco de dados de acordo com os valores adicionados no HTML através das áreas que reservei para isso e estilizei com CSS.
+  Com esse trabalho eu entendi um pouco mais de PHP conheci um pouco melhor a linguagem.
+Pratiquei conceito básico de HTML e CSS.
+Entendi como funciona as contas de porcentagem no PHP.
+ 
+ ## processo
+ Este sistema ainda não está pronto. O que ja está ponto é as paginas de login onte o usuário pode fazer o login colocando  email e senha a pagina está estilizada com css foi colocado cor, borda, as caixas de texto para inserir senha e email e os botoes de login e cadastrar-se. Os mesmos elementos foram colocados na pagina register incluindo o campo nome e um campo de para especificar se o usuário é Admim, gestor, colasborador
+
+
+ 
+ 
+ 
+# autores
+ 
+[Ana Paula](https://github.com/anapaulacd)
+ 
+orientador [Leonardo](https://github.com/LeonardoRochaMarista)
--- views/register.php
+++ views/register.php
@@ -4,12 +4,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar-se</title>
+<style>
+/* Corpo da pagina */
+body {
+    text-align: center;
+    background: rgb(140, 216, 222);
+    background: radial-gradient(circle, rgba(140, 216, 222, 1) 0%, rgba(20, 41, 193, 1) 100%);
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+main {
+    width: 350px;
+    padding: 30px;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
+/* Fomulario da pagina */
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+}
+
+input{
+    width: 100%;
+    padding: 10px;
+    margin: 10px 0;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+}
+/* Botão */
+button {
+    width: 100%;
+    padding: 10px;
+    margin-top: 20px;
+    background-color: #1429c1;
+    color: white;
+    border: none;
+    border-radius: 5px;
+   
+}
+
+
+
+a {
+    display: block;
+    margin-top: 20px;
+    color: #1429c1;
+    text-decoration: none;
+}
+
+
+</style>
 </head>
 <body>
-   <div>
+<div>
     <h2>Cadastro de usuário</h2>
     <form action="" method="post">
-        <Label for="name"Nome</Label>
+
+        <label for="name">nome</label>
         <input type="text" name id="name" require>
 
         <label for="email">Email</label>
@@ -20,9 +83,11 @@
 
         <label for="perfil">Perfil:</label>
         <select name="perfil" id="perfil">
-            <opton value="admin">Admin</opton>
-            <opton value="gestor">Gestor</opton>
-            <opton value="colaborador">Colaborador</opton>
+            <option value = "admin">Admin</option>
+            <option value = "gestor">gestor</option>
+            <option value = "colaborador">colaborador</option>
+          
+            
         </select>
         <button type="submit">Cadastrar</button>
 
@@ -34,3 +99,5 @@
 
 </body>
 </html>
+<body>
+   
